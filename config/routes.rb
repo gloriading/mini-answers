b4c0a511916ef946b5adcb48e1abf70f46a914e0
Gloria Ding <gloria.hc.ding@gmail.com>
2018-03-27 19:59:24 -0700

    created answers controller and did routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 
   get '/', to: "welcome#index", as: :home
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
   resources :users
   resource :session, only: [:new, :create, :destroy]
 
